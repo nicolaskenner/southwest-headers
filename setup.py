84a7bb25e445090d7553bd9ae99a7001ed0eb5af
Nicolas Kenner <nick@nicolaskenner.com>
2022-04-27 15:48:08 -0700

    add package to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='southwest-headers',
     version='1.0',
-    packages=[''],
+    packages=['southwest-headers'],
     url='https://github.com/nicolaskenner/southwest-headers',
     license='',
     author='nicolaskenner',
