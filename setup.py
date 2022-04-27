57dfdaf99cf2da6c12dc250034836785abd9b044
Nicolas Kenner <nick@nicolaskenner.com>
2022-04-27 15:43:03 -0700

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='southwest-headers',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/nicolaskenner/southwest-headers',
+    license='',
+    author='nicolaskenner',
+    author_email='nick@picnicpeaks.com',
+    description='Southwest Headers'
+)
